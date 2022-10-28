23502b9c6b7bf43243daaddcea951612b980f469
Kevin Umayam <kevin.m.umayam@gmail.com>
2022-10-27 21:26:47 -0700

    deleted file.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -22,6 +22,7 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
+      new CleanWebpackPlugin(),
       //custom service work
       new InjectManifest({
         swSrc: "./src-sw.js",
